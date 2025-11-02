dec14bcb0c8b83bf9da42eaf0ad8cd588a9a4ceb
Motolani <alebiosumotolani@gmail.com>
2025-11-02 17:26:31 +0100

    Write complex queries with join

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,48 @@
+SELECT 
+    properties.id AS property_id,
+    properties.name AS property_name,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM 
+    properties
+LEFT JOIN 
+    reviews
+ON 
+    properties.id = reviews.property_id;
+
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM 
+    users
+FULL OUTER JOIN 
+    bookings
+ON 
+    users.id = bookings.user_id;
+
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM users
+LEFT JOIN bookings ON users.id = bookings.user_id
+
+UNION
+
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM users
+RIGHT JOIN bookings ON users.id = bookings.user_id;
