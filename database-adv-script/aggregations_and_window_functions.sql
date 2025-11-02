3c19a949bec460c400dcd710900bf7cf0a4bb71c
Motolani <alebiosumotolani@gmail.com>
2025-11-02 19:19:52 +0100

    Apply Aggregations and Window Functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -17,7 +17,7 @@ SELECT
     p.id AS property_id,
     p.name AS property_name,
     COUNT(b.id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
 FROM 
     properties p
 LEFT JOIN 
@@ -25,6 +25,5 @@ LEFT JOIN
 ON 
     p.id = b.property_id
 GROUP BY 
-    p.id, p.name
-ORDER BY 
-    booking_rank;
+    p.id, p.name;
+
